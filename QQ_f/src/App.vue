81e8bc4ef1f9b33082689723479c19c4ca4b7796
karmen <166833697+LuciusKarmen@users.noreply.github.com>
2025-06-13 10:44:38 +0800

    6.13.1

---
+++ QQ_f/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>猜数字小游戏</h1>
+    <p>猜一个1到100的数字：</p>
+    <input v-model="guessNumber" type="number" />
+    <button @click="checkGuess">猜！</button>
+    <p v-if="message">{{message}}</p>
+  </div>
+</template>
+<script setup>
+import { ref } from 'vue';
+const guessNumber = ref(null);
+const message = ref('');
+const checkGuess = () => {
+  const randomNumber = Math.floor(Math.random() * 100) + 1;
+  if (guessNumber.value === randomNumber) {
+    message.value = '恭喜你，猜对了！';
+  } else if (guessNumber.value < randomNumber) {
+    message.value = '你猜小了，请再试一次。';
+  } else {
+    message.value = '你猜大了，请再试一次。';
+  }
+};
+</script>
